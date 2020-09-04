03a1dbf9f5bca50a768fe5b442d3693d13184d3b
jofro9 <jfroeli9@gmail.com>
2020-09-04 16:05:18 -0600

    questions 1 and 2 in appropriate files

--- hw1/question1.R
+++ hw1/question1.R
@@ -3,20 +3,4 @@
 # BIOS 6611
 # 09/10/2020
 
-### Problem 1 ###
-
-# Part A #
-library(datasets)
-library(dplyr)
-sleep <- datasets::sleep
-head(sleep)
-
-sleep1 <- filter(sleep, sleep$group == "1")
-sleep2 <- filter(sleep, sleep$group == "2")
-t.test(sleep1$extra, sleep2$extra)
-
-# Part B #
-t.test(sleep1$extra, sleep2$extra, paired = TRUE)
-
-# Part C #
-
+### Problem 2 ###
--- hw1/question2.R
+++ hw1/question2.R
@@ -3,4 +3,20 @@
 # BIOS 6611
 # 09/10/2020
 
-### Problem 2 ###
+### Problem 1 ###
+
+# Part A #
+library(datasets)
+library(dplyr)
+sleep <- datasets::sleep
+head(sleep)
+
+sleep1 <- filter(sleep, sleep$group == "1")
+sleep2 <- filter(sleep, sleep$group == "2")
+t.test(sleep1$extra, sleep2$extra)
+
+# Part B #
+t.test(sleep1$extra, sleep2$extra, paired = TRUE)
+
+# Part C #
+
